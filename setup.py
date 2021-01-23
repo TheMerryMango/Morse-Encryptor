78b73b0f60b86a8fad197e4dcba910f99bc5084b
Wyatt Sell <profwyattb@gmail.com>
2021-01-23 17:08:35 +0000

    Added config for PyPI package

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name='morse-encode-example',
+    version='1.0.14',
+    author="Jonny Parker",
+    author_email="jonny.yatesfield@gmail.com",
+    packages=["morse-encode-example"],
+    install_requires=["requests"],
+    license='MIT License',
+    description='A very bad Morse 'translator'. ,
+    url="https://github.com/TheMerryMango/Morse-Encryptor",
+    long_description=open('README.md', encoding="utf8").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+    ]
+)
